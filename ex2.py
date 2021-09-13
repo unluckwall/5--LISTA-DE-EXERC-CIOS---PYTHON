bf7a46ebcfab3e98267996b8f65a5538951bff07
Victor Hugo <victorzk26@gmail.com>
2021-09-13 20:39:06 -0300

    Create ex2.py

---
+++ ex2.py
@@ -0,0 +1,9 @@
+#2) Escreva um algoritmo que receba 10 valores digitados pelo usuário e no final exiba o maior número.
+
+cont = 1
+numb = 0
+
+while cont <= 10:
+    num = int(input('Digite um número: '))
+    numb = numb + num
+    cont += 1
